a9bf7ca89875ff82e8216436c311ffa2a59048c5
RaiuXL <ntr.impreza@gmail.com>
2026-03-19 06:41:58 -0700

    config for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    allowedHosts: ["frontend","localhost","127.0.0.1"],
+    host: "0.0.0.0",
+    allowedHosts: ["frontend","localhost","127.0.0.1","orangepizero3.taile19edd.ts.net"],
+    proxy: {
+      "/api": {
+        target: "http://api:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   test: {
     environment: "jsdom",
-    include: ["src/**/*.test.{js,jsx}"]
-  }
+    include: ["src/**/*.test.{js,jsx}"],
+  },
 });
