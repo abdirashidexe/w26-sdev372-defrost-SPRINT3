241091b237e61ffb4c6a629fc81766a9a68e4b5a
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2026-03-19 12:00:50 -0700

    email feature

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@ import pool from "./db/config.js";
 import express from "express";
 import cors from "cors";
 import twilio from "twilio";
+import { Resend } from "resend";
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const resend = new Resend('re_QaEfBqaf_BAD5fgzLJJNFfsn6Q8LPyyHu');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -49,17 +52,50 @@ app.post("/users", async (req, res) => {
   }
 });
 
-app.post("/send-text", async (req,res) => {
-    const message = await client.messages.create({
-      body: "Defrost detected! Set an earlier alarm for tommorow!",
-      messagingServiceSid: 'MG445aabc3d69bafeae5a179c6716a3f66',
-      to: '+18777804236'
-      // from: `${twilioNumber}`,
-      // to: `+1${phone}`,
-    });
-    console.log(message);
-    return res.status(200).json({message: message})
-})
+// app.post("/send-text", async (req,res) => {
+//   try{
+//     const message = await client.messages.create({
+//       body: "Defrost detected! Set an earlier alarm for tommorow!",
+//       // messagingServiceSid: 'MG445aabc3d69bafeae5a179c6716a3f66',
+//       to: '+12533350973',
+//       from: '+18556339571'
+//       // from: `${twilioNumber}`,
+//       // to: `+1${phone}`,
+//     });
+//     console.log(message);
+//     return res.status(200).json({message: message})
+//   } catch(err) {
+//     console.log(`Uh oh.. error: ${err}`)
+//   }
+// })
+
+// app.post("/send-text", async (req, res) => {
+//   const response = await fetch('https://textbelt.com/text', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//       phone: '+12533350973', // your real number
+//       message: 'Defrost detected! Set an earlier alarm for tomorrow!',
+//       key: 'textbelt', // this is the free tier key, gives 1 text/day
+//     }),
+//   });
+
+//   const data = await response.json();
+//   console.log(data);
+//   return res.status(200).json({ message: data });
+// });
+
+app.post("/send-text", async (req, res) => {
+  const data = await resend.emails.send({
+    from: 'onboarding@resend.dev', // use this exact address for testing
+    to: 'ahmed.abdirashid@student.greenriver.edu', // your real email
+    subject: 'Defrost Alert!',
+    html: '<p>Defrost detected! Set an earlier alarm for tomorrow!</p>',
+  });
+
+  console.log(data);
+  return res.status(200).json({ message: data });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
